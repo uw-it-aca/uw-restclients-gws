a44e5b43f9eab2cf8c9d70a326b6c9d77c0367dd
Jim Laney <jlaney@uw.edu>
2022-06-01 08:43:30 -0700

    optional django settings

--- examples/add_members.py
+++ examples/add_members.py
@@ -1,13 +1,16 @@
-from commonconf.backends import use_configparser_backend
+from commonconf.backends import use_configparser_backend, use_django_backend
 from commonconf import settings
 import argparse
 import os
 
 
 def add_members(group_id, file_path, act_as=None):
-    settings_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                 'settings.cfg')
-    use_configparser_backend(settings_path, 'GWS')
+    try:
+        use_django_backend()
+    except ImportError:
+        settings = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                'settings.cfg')
+        use_configparser_backend(settings, 'GWS')
 
     from uw_gws import GWS
 
@@ -23,7 +26,7 @@ def add_members(group_id, file_path, act_as=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('group_id', help='Add members for group [group_id]')
-    parser.add_argument('path', help='Path to file containing members')
+    parser.add_argument('path', help='Path to file containing members, one per line')
     parser.add_argument('--act_as', default=None, help='User to act as when adding members')
     args = parser.parse_args()
     add_members(args.group_id, args.path, args.act_as)
